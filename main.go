101b1e8d0ee1b59cc5579690b8ea20b79a8981d6
wazadio <junidio306@gmail.com>
2021-12-16 20:35:27 +0700

    admn network management sim

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/j03hanafi/bankiso/iso20022/pacs"
@@ -115,6 +117,9 @@ func biller(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("trxType:", trxType)
 
 	switch trxType {
+	case "000":
+		payload := networkRequest(body)
+		responseFormatter(w, payload, 200)
 	// ##################### Account Enquiry ##################################
 	case "510":
 		var CrAccId string
@@ -279,19 +284,21 @@ func biller(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("721")
 		//============================================================================
 	}
+	if trxType != "000" {
+		fileName = "xml/" + fileName
+		fmt.Println("filename:", fileName)
 
-	fileName = "xml/" + fileName
-	fmt.Println("filename:", fileName)
+		file, _ := os.Open(fileName)
+		defer file.Close()
 
-	file, _ := os.Open(fileName)
-	defer file.Close()
+		response, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	response, err := ioutil.ReadAll(file)
-	if err != nil {
-		log.Fatal(err)
+		responseFormatter(w, response, 200)
 	}
 
-	responseFormatter(w, response, 200)
 }
 
 func responseFormatter(w http.ResponseWriter, data []byte, statusCode int) {
@@ -305,3 +312,46 @@ func responseFormatter2(w http.ResponseWriter, data []byte, statusCode int) {
 	w.WriteHeader(statusCode)
 	w.Write(data)
 }
+
+func networkRequest(data []byte) []byte {
+	request := Admn1{}
+	err := xml.Unmarshal(data, &request)
+	if err != nil {
+		fmt.Println("error")
+	}
+
+	fmt.Println(request.Document.AdmnReq.AdmnTxInf.FnctnCd)
+
+	response := Admn2{}
+	response.Ns = "urn:iso"
+	response.Ns1 = "urn:iso:std:iso:20022:tech:xsd:head.001.001.01"
+	response.Ns2 = "urn:iso:std:iso:20022:tech:xsd:admn.002.001.01"
+	response.Xsi = "http://www.w3.org/2001/XMLSchema-instance"
+	response.SchemaLocation = "urn:iso ../../../../xsd/phase1/MainCIHub.xsd "
+
+	response.AppHdr.Fr.FIId.FinInstnId.Othr.ID = request.AppHdr.Fr.FIId.FinInstnId.Othr.ID
+	response.AppHdr.To.FIId.FinInstnId.Othr.ID = request.AppHdr.To.FIId.FinInstnId.Othr.ID
+	response.AppHdr.BizMsgIdr = request.AppHdr.BizMsgIdr
+	response.AppHdr.MsgDefIdr = "admn.002.001.01"
+	response.AppHdr.CreDt = time.Now().Format("2006-01-02T15:04:05.000")
+
+	response.Document.AdmnResp.GrpHdr.MsgId = time.Now().Format("2006-01-02T15:04:05Z07:00")[0:4] + time.Now().Format("2006-01-02T15:04:05Z07:00")[5:7] + time.Now().Format("2006-01-02T15:04:05Z07:00")[8:10] + ("INDOIDJA" + fmt.Sprint(rand.Intn(9)) + fmt.Sprint(rand.Intn(9)) + fmt.Sprint(rand.Intn(9)) + fmt.Sprint(rand.Intn(9)) +
+		fmt.Sprint(rand.Intn(9)) + fmt.Sprint(rand.Intn(9)) + fmt.Sprint(rand.Intn(9)) + fmt.Sprint(rand.Intn(9)))
+	response.Document.AdmnResp.GrpHdr.CreDtTm = time.Now().Format("2006-01-02T15:04:05.000")
+	response.Document.AdmnResp.AdmnResponse.InstgAgt.FinInstnId.Othr.ID = "NOBUIDJA"
+	response.Document.AdmnResp.AdmnResponse.OrgnlInstrId = request.Document.AdmnReq.AdmnTxInf.InstrId
+	response.Document.AdmnResp.AdmnResponse.FnctnCd = request.Document.AdmnReq.AdmnTxInf.FnctnCd
+	response.Document.AdmnResp.AdmnResponse.TxSts = "ACTC"
+
+	payload, err := xml.Marshal(response)
+	if err != nil {
+		fmt.Println("error")
+	}
+	fmt.Println(string(payload))
+
+	return payload
+}
+
+// func main() {
+// 	tes()
+// }
